b882277814317e3524ac3d0f07763e90b79ae476
GlenJAllen <glen.j.o.allen@gmail.com>
2018-01-25 12:18:31 -0500

    cleaned this up a bit

--- pull_clean_data.R
+++ pull_clean_data.R
@@ -4,14 +4,9 @@ library(magrittr)
 
 setwd("C:/Users/u55a20/Downloads/jhu_eda")
 
-household.power.consumption <- fread("household_power_consumption.txt",
-                                     na.strings = "?")
+suppressMessages(fread("household_power_consumption.txt", na.strings = "?") %>%
+                   mutate(Date = as.Date(Date, "%d/%m/%Y")) %>%
+                   filter(between(Date, as.Date("2007-02-01"), as.Date("2007-02-02"))) %>%
+                   attach())
 
-household.power.consumption %<>% mutate(Date = as.Date(Date, "%d/%m/%Y")) %>%
-  filter(between(Date, as.Date("2007-02-01"), as.Date("2007-02-02")))
-
-invisible(attach(household.power.consumption))
-
-Date <- strptime(paste(household.power.consumption$Date,
-                       household.power.consumption$Time),
-                 "%Y-%m-%d %H:%M:%S")
+Date <- strptime(paste(Date, Time), "%Y-%m-%d %H:%M:%S")
