b3035c822fe584ec4d917634422d0019c9eb0bf5
GlenJAllen <glen.j.o.allen@gmail.com>
2018-01-25 12:25:18 -0500

    Update pull_clean_data.R

--- pull_clean_data.R
+++ pull_clean_data.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(dplyr)
 
+# Assumes you've already downloaded the data and have it in this folder
 suppressMessages(fread("household_power_consumption.txt", na.strings = "?") %>%
                    mutate(Date = as.Date(Date, "%d/%m/%Y")) %>%
                    filter(between(Date, as.Date("2007-02-01"), as.Date("2007-02-02"))) %>%
