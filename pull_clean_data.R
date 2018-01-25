2a33bf5920743d7c850113a06c3265c8846cd8f9
GlenJAllen <glen.j.o.allen@gmail.com>
2018-01-25 12:32:04 -0500

    Add files via upload

--- pull_clean_data.R
+++ pull_clean_data.R
@@ -1,10 +1,9 @@
 library(data.table)
 library(dplyr)
 
-# Assumes you've already downloaded the data and have it in this folder
-suppressMessages(fread("household_power_consumption.txt", na.strings = "?") %>%
-                   mutate(Date = as.Date(Date, "%d/%m/%Y")) %>%
-                   filter(between(Date, as.Date("2007-02-01"), as.Date("2007-02-02"))) %>%
-                   attach())
+fread("household_power_consumption.txt", na.strings = "?") %>%
+  mutate(Date = as.Date(Date, "%d/%m/%Y")) %>%
+  filter(between(Date, as.Date("2007-02-01"), as.Date("2007-02-02"))) %>%
+  attach(warn.conflicts = FALSE)
 
 Date <- strptime(paste(Date, Time), "%Y-%m-%d %H:%M:%S")
